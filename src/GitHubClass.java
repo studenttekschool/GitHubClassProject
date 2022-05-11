fc2dc254b879beb78cb3a6c33760f4bcd6c9857e
unknown <nasim.haleemi@tekschool.us>
2022-05-10 23:39:25 -0400

    Class practice

---
+++ src/GitHubClass.java
@@ -0,0 +1,13 @@
+
+public class GitHubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 10;
+		int c= a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/practiceclass.java
@@ -0,0 +1,10 @@
+
+public class practiceclass {
+
+	
+		int f = 0;
+		{
+			System.out.println(f);
+			while(f<9);
+	}
+}
