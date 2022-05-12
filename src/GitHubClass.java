a4d2622d19be293f7e3e8694519b4ee3d5c0fe1f
unknown <nasim.haleemi@tekschool.us>
2022-05-12 00:08:27 -0400

    This is GitHubClass

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -3,7 +3,11 @@ public class GitHubClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 
+		int a =10;
+		int b=10;
+		int c = a+b;
+		System.out.println("Sum of a and b is : "+ c);
 	}
 
 }
--- src/NewClass.java
+++ src/NewClass.java
@@ -3,7 +3,9 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		 *  This is NewClass..See it 
+		 */
 	}
 
 }
