5e10ea2150a87017f361198d054a7c5112d10279
unknown <nasim.haleemi@tekschool.us>
2022-05-12 19:19:28 -0400

    Lab Session first commit

--- src/LabSessionClass.java
+++ src/LabSessionClass.java
@@ -1,9 +1,9 @@
 
-public class SharksClass {
+public class LabSessionClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is amazing class");
+
 	}
 
 }
