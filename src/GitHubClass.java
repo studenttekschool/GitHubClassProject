67b1d19e37d1c9d5663b5b2ef53cedc2a5a16d45
unknown <nasim.haleemi@tekschool.us>
2022-05-11 23:23:23 -0400

    class practice

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -3,10 +3,6 @@ public class GitHubClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 10;
-		int b = 10;
-		int c= a+b;
-		System.out.println(c);
 
 	}
 
